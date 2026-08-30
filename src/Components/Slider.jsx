53b0c5c4a3b8510a9602873850d0f3a32b5bc65c
Kevin Brewster <kbrewster81@gmail.com>
2026-08-29 22:32:53 -0400

    First commit

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -3,7 +3,7 @@ import GlobalApi from "../Services/GlobalApi";
 import scrollbarHide from 'tailwind-scrollbar-hide';
 import { HiChevronLeft, HiChevronRight  } from "react-icons/hi2";
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
-
+const screenWidth=window.innerWidth;
 const Slider = () => {
   const [movieList, setMovieList] = useState([]);
   const elementRef=useRef();
@@ -16,23 +16,24 @@ const Slider = () => {
       setMovieList(resp.data.results);
     });
   };
-  const sliderRight=(element)=>{
-    element.scrollLeft+=800
+  const slideRight=(element)=>{
+    element.scrollLeft+=screenWidth-110
   }
-  const sliderLeft=(element)=>{
-    element.scrollLeft-=800
+  const slideLeft=(element)=>{
+    element.scrollLeft-=screenWidth-110
   }
   return (
     <div>
-      <HiChevronLeft className="hidden md: block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer"
-      onClick={()=>sliderLeft(elementRef.current)}/>
-      <HiChevronRight className="hidden md: block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"
-      onClick={()=>sliderRight(elementRef.current)}/>
+      <HiChevronLeft className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer"
+      onClick={()=>slideLeft(elementRef.current)}/>
+      <HiChevronRight className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"
+      onClick={()=>slideRight(elementRef.current)}/>
 
       <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-hide scroll-smooth'ref={elementRef}>
         {movieList.map((item)=> (
             <img src={IMAGE_BASE_URL + item.backdrop_path}
-            className="min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md"/>
+            className="min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] 
+            border-gray-400 transition-all duration-100 ease-in"/>
         ))};
       </div>
     </div>
